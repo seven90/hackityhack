4b70e02eb1ac113494872bc22c024e8384e65836
Ashley <ashleykwtam@gmail.com>
2014-06-21 14:01:27 -0400

    controllers

--- angelhack/app/controllers/students_controller.rb
+++ angelhack/app/controllers/students_controller.rb
@@ -15,9 +15,11 @@ class StudentsController < ApplicationController
     end
   end
 
+  
+
   private
   def student_params
     params.require(:student).permit(:first_name, :last_name, :email, :password, :password_confirmation, :period_1, :period_2, :period_3, :period_4, :period_5, :afterschool)
   end
-  
+
 end
