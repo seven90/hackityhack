ee98ab231904f154f15051bb633e746095c3c9b2
Ashley <ashleykwtam@gmail.com>
2014-06-21 13:58:03 -0400

    students controller and views

--- angelhack/app/controllers/students_controller.rb
+++ angelhack/app/controllers/students_controller.rb
@@ -1,4 +1,23 @@
 class StudentsController < ApplicationController
   def index
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student
+      redirect_to students_path, :notice => "Signed Up!"
+    else 
+      render :new
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email, :password, :password_confirmation, :period_1, :period_2, :period_3, :period_4, :period_5, :afterschool)
+  end
+  
 end
