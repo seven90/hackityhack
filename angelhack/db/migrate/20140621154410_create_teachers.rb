3ea701a72b11c4612c370372144db46411bc51ab
Seven90 <stevesurya@gmail.com>
2014-06-21 13:14:21 -0400

    Created lessons model

---
+++ angelhack/db/migrate/20140621154410_create_teachers.rb
@@ -0,0 +1,14 @@
+class CreateTeachers < ActiveRecord::Migration
+  def change
+    create_table :teachers do |t|
+
+    t.string :first_name
+    t.string :last_name
+    t.string :email
+    t.string :password_digest 
+    t.string :course
+
+    t.timestamps
+  end
+  end
+end
