6b5260c29746470363478dcb5efd54882e70284b
Ashley <ashleykwtam@gmail.com>
2014-06-21 14:32:11 -0400

    student sessions and view

--- angelhack/app/controllers/student_sessions_controller.rb
+++ angelhack/app/controllers/student_sessions_controller.rb
@@ -2,4 +2,23 @@ class StudentSessionsController < ApplicationController
   def index
   end
   
+  def new
+  end
+
+  def create
+    student = Student.find_by_email(params[:email])
+    if student && student.authenticate(params[:password])
+      session[:student_id] = student.id
+      redirect_to lessons_path, :notice => "Logged in!"
+    else
+      render "root_path" 
+    end
+      
+  end
+
+  def destroy
+    session[:student_id] = nil
+    redirect_to root_path :notice => "Logged out!"
+  end
 end
+
--- angelhack/app/controllers/teacher_sessions_controller.rb
+++ angelhack/app/controllers/teacher_sessions_controller.rb
@@ -1,4 +1,25 @@
 class TeacherSessionsController < ApplicationController
   def index
   end
+  
+  def new
+  end
+
+  def create
+    teacher = Teacher.find_by_email(params[:email])
+    if teacher && teacher.authenticate(params[:password])
+      session[:teacher_id] = teacher.id
+      redirect_to lessons_path, :notice => "Logged in!"
+    else
+      render "root_path" 
+    end
+      
+  end
+
+  def destroy
+    session[:teacher_id] = nil
+    redirect_to root_path :notice => "Logged out!"
+  end
+end
+
 end
