19a1fca63c4eb4fd2d07ec280ac492055be3566f
Ashley <ashleykwtam@gmail.com>
2014-06-21 16:38:59 -0400

    Home page login css and javascript added

--- angelhack/app/assets/javascripts/pages.js.coffee
+++ angelhack/app/assets/javascripts/pages.js.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on("ready page:load", ->
+  $("#select-user-type").on("click", "a", (event) ->
+    event.preventDefault()
+    selection = $(@)
+    $("#user-type-status").text(selection.text())
+    $("#new-user-form").attr("action", selection.data("action"))
+    $('')
+  )
+)
--- angelhack/app/controllers/application_controller.rb
+++ angelhack/app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
-  private
 
-  def current_teacher
-    @current_teacher ||= Teacher.find(session[:teacher_id]) if session[:teacher_id]
-  end
-
-  def current_student
-    @current_student ||= Student.find(session[:student_id]) if session[:student_id]
-  end
-
-  helper_method :current_teacher, :current_student
 end
