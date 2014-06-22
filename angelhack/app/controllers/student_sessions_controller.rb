9ae450180f08c0028b879bdd543e14d8c76907ab
Seven90 <stevesurya@gmail.com>
2014-06-22 04:09:17 -0400

    Working, now just need to stylize

--- angelhack/app/controllers/student_sessions_controller.rb
+++ angelhack/app/controllers/student_sessions_controller.rb
@@ -10,7 +10,7 @@ class StudentSessionsController < ApplicationController
     student = Student.find_by_email(params[:email])
     if student && student.authenticate(params[:password])
       session[:student_id] = student.id
-      redirect_to lessons_path, :notice => "Logged in!"
+      redirect_to students_path, :notice => "Logged in!"
     else
       render "root_path" 
     end
