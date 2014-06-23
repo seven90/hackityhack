3df0b2d6fd55447f070d27f466468e7bc8d32520
Seven90 <stevesurya@gmail.com>
2014-06-23 16:59:42 -0400

    Finished teacher view with styling

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -16,6 +16,11 @@ def create
   end
 end
 
+def show
+  @lessons = Lesson.all
+  @votes = Vote.all
+end
+
 private
 def teacher_params
   params.require(:teacher).permit(:first_name, :last_name, :email, :password, :password_confirmation, :subject)
